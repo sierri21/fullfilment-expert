fbb8bece6500682f7dac504d4b5ad7deba939f38
s.zolotnitskii <s.zolotnitskii@icloud.com>
2025-12-18 13:18:20 +0300

    добавил карты

--- src/sections/footer/s-footer.vue
+++ src/sections/footer/s-footer.vue
@@ -16,7 +16,7 @@ footer.footer
             a.text--white( href="mailto:fulfilexcpert222@gmail.com" ) fulfilexcpert222@gmail.com
             a.text--white( href="tel:+79697199669" ) +7 (969) 719-96-69
             a.text--white( href="tel:+79657556556" ) +7 (965) 755-65-56
-            .text--white Санкт – Петербург, Московское шоссе 162
+            a.text--white( href="https://yandex.ru/maps/-/CLwlmJoa" ) Санкт – Петербург, Московское шоссе 162
 </template>
 <script setup>
 import ITelegram from "./socials/i-telegram.vue";
@@ -52,6 +52,7 @@ import IImo from "~/sections/footer/socials/i-imo.vue";
             text-align: right;
             a {
                 font-size: var(--text-size-large);
+                cursor: pointer;
                 &:hover {
                     text-decoration: underline;
                 }
